cbd4ba7df0f12c8056de0c126127eabf63a46d73
Jesse Kela <jessekkela@gmail.com>
2024-08-01 16:45:15 +0300

    Move files in project

--- util/http_server.py
+++ util/http_server.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
+import os
 
 PORT = 8000
 
@@ -9,6 +10,10 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Origin", "*")
         http.server.SimpleHTTPRequestHandler.end_headers(self)
 
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+os.chdir(parent_dir)
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
